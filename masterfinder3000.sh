fbb8d6bf6a8243689a914042c0aed83a7bab5159
Viktor Edman <viktor@edman.se>
2022-11-21 10:16:56 +0100

    Update masterfinder3000.sh
    Added functionality to print out serial number for each master found

--- masterfinder3000.sh
+++ masterfinder3000.sh
@@ -1,5 +1,6 @@
-arp -a | awk '! /IP/ {print "http://"$1"/"}' | while read line; do
-    if $(curl -v --silent $line 2>&1 | grep -q "Forgot your password?"); then
-        echo "$line is a Master"
+arp -a | awk '! /IP/ {print "http://"$1}' | while read line; do
+    RESPONSE=$(curl -v --silent $line 2>&1 | grep -A 1 title | grep -E -o '[0-9]{8,8}' | awk '{print $1}')
+    if [ $(echo $RESPONSE | awk '{print length}') -gt 0 ]; then
+        echo "Master with serial number" $RESPONSE "is at URI" $line
     fi
 done
